2d40fd55dfd746c7074908c563387f089be58700
Adia Mahardika <dikaadia@gmail.com>
2020-07-23 10:14:12 +0700

    Integration Testing get search_caegory_name and get detail news-category API

--- test/api/api_news_category_test.js
+++ test/api/api_news_category_test.js
@@ -54,28 +54,64 @@ describe("News Category API", () => {
         });
     });
   }),
-  describe("/GET news category", () => {
-    it("It should GET all news category", (done) => {
-      checkNewsCategoryName(data.news_category_name), 
-      chai
-          .request(api)
-          .get("/")
-          .end((error, response) => {
-            expect(response.body).to.have.status(200);
-            expect(response.body).to.have.property("result");
-            expect(response.body.result[0]).to.have.property("id");
-            expect(response.body.result[0])
-              .to.have.property("news_category_name")
-              .equal(data.news_category_name);
-            expect(response.body.result[0]).to.have.property("date_created");
-            expect(response.body.result[0]).to.have.property("date_updated");
-            done();
-          });
-      });
+    describe("/GET news category", () => {
+      it("It should GET all news category", (done) => {
+        checkNewsCategoryName(data.news_category_name),
+          chai
+            .request(api)
+            .get("/")
+            .end((error, response) => {
+              expect(response.body).to.have.status(200);
+              expect(response.body).to.have.property("result");
+              expect(response.body.result[0]).to.have.property("id");
+              expect(response.body.result[0])
+                .to.have.property("news_category_name")
+              expect(response.body.result[0]).to.have.property("date_created");
+              expect(response.body.result[0]).to.have.property("date_updated");
+              done();
+            });
+      }),
+        it("It should GET search news category name", (done) => {
+          checkNewsCategoryName(data.news_category_name),
+            chai
+              .request(api)
+              .get(`/?search_category_name=${data.news_category_name}`)
+              .end((error, response) => {
+                expect(response.body).to.have.status(200);
+                expect(response.body).to.have.property("result");
+                expect(response.body.result[0]).to.have.property("id");
+                expect(response.body.result[0])
+                  .to.have.property("news_category_name")
+                  .equal(data.news_category_name);
+                expect(response.body.result[0]).to.have.property(
+                  "date_created"
+                );
+                expect(response.body.result[0]).to.have.property(
+                  "date_updated"
+                );
+                done();
+              });
+        }),
+        it("It should GET detail news category", async () => {
+          const id = await x[0].id;
+          chai
+            .request(api)
+            .patch("/" + id)
+            .end((error, response) => {
+              expect(response.body).to.have.status(200);
+              expect(response.body).to.have.property("result");
+              expect(response.body.result[0]).to.have.property("id");
+              expect(response.body.result[0]).to.have.property(
+                "news_category_name"
+              );
+              expect(response.body.result[0]).to.have.property("date_created");
+              expect(response.body.result[0]).to.have.property("date_updated");
+            });
+        });
     }),
     describe("/PATCH news category", () => {
-      it("it should PATCH news category", async() => {
-        const id = await x[0].id
+      it("It should PATCH news category", async () => {
+        const id = await x[0].id;
         chai
           .request(api)
           .patch("/" + id)
@@ -93,8 +129,8 @@ describe("News Category API", () => {
       });
     }),
     describe("/DELETE news category", () => {
-      it("it should DELETE news category", async () => {
-        const id = await x[0].id
+      it("It should DELETE news category", async () => {
+        const id = await x[0].id;
         chai
           .request(api)
           .delete("/" + id)
